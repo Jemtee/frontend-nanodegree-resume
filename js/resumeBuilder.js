11e4b284a9acec6bd5579fa3f671761af3c74ff8
Jemtee <andreas.nordin@hyperisland.se>
2017-03-16 09:55:17 +0100

    -

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var name = 'Andreas Nordin'
+var role = 'Rhynocerous'
+
+
+var formattedName = HTMLheaderName.replace('%data%', name);
+var formattedRole = HTMLheaderName.replace('%data%', role);
+
+$('#header').prepend(formattedRole);
+$('#header').prepend(formattedName + '<br>');
